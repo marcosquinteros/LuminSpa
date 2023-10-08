2970acab4c51abce0e9c60d20e6275486ef51053
mwarcos <mmwarcos@gmail.com>
2023-10-08 19:54:53 -0300

    correccion de importaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Footer from "./Components/Layout/Footer";
+import Navbar from "./Components/Layout/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
         </Routes>
         <Footer pathIndex="/Inicio" pathNosotros="/Nosotros" pathError="/*" />
       </Router>
-         
     </div>
-      
-  )
+  );
 }
 
-export default App
+export default App;
