b019f6c61af5fbbba7812dd41201118437a902ab
Paul Urcan <paulurcan@gmail.com>
2023-10-18 00:47:34 -0300

    Avances en Store

---
+++ src/Components/Store/Dropdown_btn.jsx
@@ -0,0 +1,27 @@
+import React, { useState }  from "react";
+
+const Dropdown_btn = () => {
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+    }
+
+    return(
+        <div className="m-3">
+            <label htmlFor="categoryDropdown">Selecciona una categoría:</label>
+            <select 
+            id="categoryDropdown"
+            className="form-select"
+            value={selectedCategory}
+            onChange={handleCategoryChange}>
+                <option value="">Elige una categoría</option>
+                <option value="cabello">Cabello</option>
+                <option value="cara">Cara</option>
+                <option value="cuerpo">Cuerpo</option>
+            </select>
+        </div>
+    )
+}
+
+export default Dropdown_btn
--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import productos from "../Components/Store/Productos";
+import Dropdown_btn from "../Components/Store/Dropdown_btn";
 
-class Store extends React.Component {
-  render() {
-    const categoria = this.props.match.params.categoria;
+const Store = () =>{
 
-    const productosFiltrados = productos.filter(
-      (producto) => producto.categoria === categoria
-    );
-
-    return (
-      <div>
-        <h2>Tienda - Categoría: {categoria}</h2>
-        <ul>
-          {productosFiltrados.map((producto, index) => (
-            <li key={index}>{producto.nombre}</li>
-          ))}
-        </ul>
+  return(
+  <div className="container-fluid column p-0">
+    <div className="container-fluid d-flex justify-content-start-lg">
+      <Dropdown_btn />
+    </div>
+    <div className="container-fluid column bg-primary">
+      <div className="container-fluid mt-5 h-75">
+        <div className="cards-container">
+          <div
+        style={{
+          overflowX: "auto",
+          scrollSnapType: "x mandatory",
+          display: "flex",
+          scrollBehavior: "smooth",
+        }}
+          className="my-5 "
+          />
+        </div>
+      <div className="container-fluid mt-5 h-75">
+        layer 2
       </div>
-    );
-  }
+    </div>
+  </div>
+</div>
+  )
 }
 
 export default Store;
