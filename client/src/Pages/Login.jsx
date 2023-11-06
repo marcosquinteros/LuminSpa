ff64f134321901bfa52f185cb956734cf46ca0fd
BigMatias <matiasjosegrande@gmail.com>
2023-11-05 22:51:55 -0300

    Register y Login terminados

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,8 +1,57 @@
+import { Link, useNavigate, useNavigation } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
     const Login = () => {
+
+      const [username, setUsername] = useState('');
+      const [password, setPassword] = useState('');
+      const usenavigate = useNavigate();
+
+      const validar = () => {
+        let result = true ;
+        if(username == null || username == ('')) {
+          result = false;
+          toast.warning('Por favor ingrese un usuario')
+        }
+        if(password == null || password == ('')) {
+          result = false;
+          toast.warning('Por favor ingrese una contraseña')
+        }
+        return result;
+      } 
+      
+      const validLogin = (e) => {
+        e.preventDefault();
+        if(validar()) {
+          
+fetch("http://localhost:3000/users/" + username).then((res)=> {
+  return res.json();
+
+}).then((resp) => {
+  if(Object.keys(resp).length===0) {
+    toast.error('Por favor ingresa un usuario válido');
+  }else{
+    if(resp.pass === password) {
+      toast.success('Login exitoso!')
+      sessionStorage.setItem('username', username);
+      usenavigate("/")
+    }else{
+      toast.error('Credenciales incorrectas');
+    }
+  }
+}).catch((err) => {
+  toast.error('El login falló por :' + err.message);
+});
+
+        }
+      }
+
+      
+
       return (
           <div className="container text-center">  
-            <div className="contenedor-register-login">           
+            <form onSubmit={validLogin} className="contenedor-register-login">           
               <div className="row gy-3">
               
               <div className="col-lg-6">
@@ -10,27 +59,40 @@
               </div>
 
               <div className="col-lg-6">
+                <div className="col-lg-12">
+                  <h1> Log in </h1>
+                  <p> Ingresá tus datos para acceder. </p>
+                </div>
                   <div className='col-lg-12'>
-                  <p className="texto-register-login"> Usuario o E-Mail </p>
-                  <input id="input" placeholder="" type="email" name="user_email" className='form-control' />
+                  <p className="texto-register-login"> Usuario </p>
+                  <input value={username} onChange={e=>setUsername(e.target.value)} id="input" type="text" className='form-control' />
                 </div>
                 <div className='col-lg-12'>
                   <p className="texto-register-login"> Contraseña </p>
-                  <input id="input" placeholder="" type="contraseña" name="user_epass" className='form-control' />
+                  <input value={password} onChange={e=>setPassword(e.target.value)} id="input" type="password"  className='form-control' />
                   <div className="col-lg-6">
                     <div className='container text-start'>
                         <input type="submit" value="Enviar" className='boton-submit' />
                     </div>
+                    </div>
+                    <div>
+                    <Link
+                        to="/register"
+                        className="btn lumin-btn"
+                    >
+                            ¿No creaste una cuenta? Apretá acá para registrarte
+                    </Link>
+
                     </div>
                 </div>
               
               </div>
 
             </div>
-          </div>
+          </form>
           </div>
       );
     };
   
 
-export default Login;
+export default Login;
--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,18 +1,78 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const RegisterC = () => {
 
+    const[name, setName] = useState("");
+    const[password, setPass] = useState("");
+    const[pass2, setPass2] = useState("");
+    const[id, setId] = useState("");
+    const[email, setEmail] = useState("");
+    const[sex, setSex] = useState(" ");
+
+    
+    const IsValid = () => {
+        let isproceed = true;
+        let errormessage = 'Todos los campos son obligatorios: ';
+        if(name == null || name=='') {
+            isproceed=false;
+            errormessage += 'Name. ' ;
+        }
+        if(id == null || id=='') {
+            isproceed=false;
+            errormessage += 'Usuario. ' ;
+        }
+        if(password == null || pass=='') {
+            isproceed=false;
+            errormessage += 'Contraseña. ' ;
+        }
+        if(pass2 == null || pass2=='') {
+            isproceed=false;
+            errormessage += 'Confirmar Contraseña. ' ;
+        }
+        if(email == null || email=='') {
+            isproceed=false;
+            errormessage += 'Email. ' ;
+        }
+        if(!isproceed){
+            toast.warning(errormessage)
+        }
+        return isproceed;
+    }
+
+    const handleSumbit=(e) => {
+        e.preventDefault();
+        let regobj={id, name, password, pass2, email, sex};
+
+        if (IsValid()) {
+
+fetch("http://localhost:3000/users", {
+    
+method: "POST",
+headers: {'content-type' : 'application/json'},
+body : JSON.stringify(regobj)
+}).then((res)=>{
+toast.success('Registro existoso: ')
+}).catch((err)=>{
+toast.error('Error:' + err.message );
+
+});
+    }
+
+        }
 
 
     return (
     <div className="container text-center">
-        <div className="contenedor-register-login">
-            <div className="contenedor-form-register">
+        <form onSubmit={handleSumbit} className="contenedor-register-login">
+            <div className="contenedor-form-register" >
             <div className="row gy-5"> 
                   <div className="col-lg-6">
                     <div>
                         <div className='col-lg-12'>
                         <p className="texto-register-login"> Nombre Completo </p>
-                        <input placeholder="" type="text" name="nombre" id="input"className='form-control' />
+                        <input  value={name} onChange={e=>setName(e.target.value)} placeholder="" type="text" name="nombre" id="input"className='form-control' />
                         </div>
                     </div>
 
@@ -20,45 +80,65 @@ const RegisterC = () => {
 
                     <div className='col-lg-12'>
                             <div className="row gy-5">
-                            <div className="form-floating">
-                                <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-                                    <option value="1">Femenino</option>
-                                    <option value="2">Masculino</option>
-                                </select>
-                                <label for="floatingSelect">Sexo</label>
+                                <div>
+                                    <label className="texto-register-login">Sexo</label>
+                                    <br />
+                                       
+                                    <input className="form-check-input" type="radio" checked={sex==='masculino'} onChange={e=>setSex(e.target.value)} name="flexRadioDefault" value="masculino" />
+                                    <label className="form-check-label" htmlFor="masculino"> Masculino </label>
                                 </div>
-                                                                
+
+                                <div>
+                                        
+                                    <input className="form-check-input" type="radio" checked={sex==='femenino'} onChange={e=>setSex(e.target.value)} name="flexRadioDefault" value="femenino" />
+                                    <label className="form-check-label" htmlFor="femenino"> Femenino </label>
+
+                                </div>
+
                             </div>
+                             
                         </div>
                     
 
                     <div className='col-lg-12'>
                         <p className="texto-register-login"> E-mail </p>
-                        <input id="input" placeholder="" type="email" name="user_email" className='form-control' />
+                        <input  value={email} onChange={e=>setEmail(e.target.value)} id="input" placeholder="" type="email" name="user_email" className='form-control' />
                     </div>
 
                     <div>    
                     <div className='col-lg-12'>
                         <p className="texto-register-login"> Usuario </p>
-                        <input id="input" placeholder='' type="text" name="username" className='form-control '/>                </div>
+                        <input  value={id} onChange={e=>setId(e.target.value)}id="input" placeholder='' type="text" name="username" className='form-control '/>                </div>
                     </div>
                     <div className='col-lg-12'>
                         <p className="texto-register-login"> Contraseña </p>
-                        <input id="input" placeholder='' type="text" name="password" className='form-control '/>                </div>
+                        <input  value={password} onChange={e=>setPass(e.target.value)} id="input" placeholder='' type="text" name="password" className='form-control '/>                </div>
                 
                     <div>
                     <div className='col-lg-6'>
                         <p className="texto-register-login"> Ingrese la contraseña de nuevo </p>
-                        <input id="input" placeholder='' type="text" name="password" className='form-control '/>                </div>
+                        <input  value={pass2} onChange={e=>setPass2(e.target.value)} id="input" placeholder='' type="text" name="password2" className='form-control '/>                </div>
                     </div>
                     <div className="col-lg-6">
                     <div className='container text-start'>
                         <input type="submit" value="Enviar" className='boton-submit' />
                     </div>
                     </div>
+                    <div className="link-register">
+                        
+                    <Link
+                        to="/login"
+                        className="btn lumin-btn px-5"
+                    >
+                        ¿Ya tenés una cuenta? Apretá acá para ingresar.
+                    </Link>
                     
                     </div>
+
+                    </div>
                     <div className="col-lg-6">
+                        <h1 className="titulo-register"> Registro </h1>
+                        <p> Brindanos un par de datos para comenzar. </p>
                         <div className="imagen-register">
                             <img src="https://res.cloudinary.com/dehmb7i5p/image/upload/v1697472156/codigo360/pexels-yan-krukau-7011186_uhjuws.jpg" alt="" /> 
                         </div>
@@ -66,7 +146,7 @@ const RegisterC = () => {
                     
             </div> 
                   </div>
-         </div>
+         </form>
     </div>
     );
   };
